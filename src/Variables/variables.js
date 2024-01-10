7e6b24b90b151a372024f0006f7bafa941b076a1
Rachid Bendaghor <rachidbendaghor@gmail.com>
2024-01-10 14:34:34 +0100

    changed the animation of the pages

--- src/Variables/variables.js
+++ src/Variables/variables.js
@@ -29,3 +29,7 @@ export const childrenVariants = {
     },
   },
 };
+
+export const API_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vZ2hzdWt4ZnpueGxtaGVuYmtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MzU2NTgsImV4cCI6MjAyMDQxMTY1OH0.ez2ULk1Z1bACj7JYvDJpDjesFAtmUBmn8EQva65MhG8';
+export const API_URL = 'https://noghsukxfznxlmhenbko.supabase.co';
