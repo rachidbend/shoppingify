80e69185e94b35629cfe54ee302d92abff857c02
Rachid Bendaghor <rachidbendaghor@gmail.com>
2024-01-18 22:00:55 +0100

    the navigation between the shopping list and the add new item page is done, but the animations still need work. #16

---
+++ src/Features/itemDetails/ItemDetails.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledItemDetails = styled.div``;
+
+function ItemDetails() {
+  return <StyledItemDetails>ItemDetails</StyledItemDetails>;
+}
+
+export default ItemDetails;
