e31f0684d0cc9a6b71faaf070b7a0d8ccad2deaa
Rachid Bendaghor <rachidbendaghor@gmail.com>
2024-01-20 13:19:23 +0100

    started on the item details page

--- src/Features/itemDetails/ItemDetails.jsx
+++ src/Features/itemDetails/ItemDetails.jsx
@@ -1,8 +1,12 @@
+import { useParams } from 'react-router';
 import styled from 'styled-components';
 
 const StyledItemDetails = styled.div``;
 
 function ItemDetails() {
+  const { itemId } = useParams();
+  console.log(itemId);
+
   return <StyledItemDetails>ItemDetails</StyledItemDetails>;
 }
 
