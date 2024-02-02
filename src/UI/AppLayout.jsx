8c08c801554b59e5dcc20fe77c20c775691af4b2
Rachid Bendaghor <rachidbendaghor@gmail.com>
2024-02-02 12:30:51 +0100

    adjusted Applayout for tablet view. #52

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -26,20 +26,32 @@ const StyledAppLayout = styled(motion.div)`
   background-color: var(--color-background);
   position: relative;
 
+  @media screen and (max-width: 780px) {
+    grid-template-columns: 6.1581rem 1fr;
+  }
+
   @media screen and (max-width: 480px) {
     grid-template-columns: 6.1581rem 1fr;
   }
 `;
 
-const SideContainer = styled.div`
+const SideContainer = styled(motion.div)`
   width: 100%;
   height: 100vh;
   position: ${props => (props.ismobile === 'true' ? 'absolute' : 'static')};
 
-  /* left: 6.1581rem; */
   left: ${props => (props.isopen === 'true' ? '6.1581rem' : '100%')};
   top: 0;
   right: 0;
+
+  transition: left 260ms cubic-bezier(0.075, 0.82, 0.165, 1),
+    right 260ms cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  @media screen and (max-width: 780px) {
+    width: 38rem;
+    left: auto;
+    right: ${props => (props.isopen === 'true' ? '0' : '-100%')};
+  }
 
   @media screen and (max-width: 480px) {
     width: calc(100% - 6.1581rem);
@@ -50,7 +62,7 @@ function AppLayout() {
   const { page, handleChangePage } = useSidePage();
   const { itemId } = useParams();
   const { isOpen } = useMobileSide();
-  const isMobile = window.innerWidth <= 480;
+  const isMobile = window.innerWidth <= 780;
 
   return (
     <StyledAppLayout>
